bca2e7a7ed422e0dc68f112d9495aa74b414186a
ayush.kumar <ayush.kumar@highradius.com>
2020-02-15 00:02:40 +0530

    ChatWindow Fixes

--- src/component/ChatWindow.js
+++ src/component/ChatWindow.js
@@ -22,24 +22,28 @@ export default class ChatWindow extends Component {
                     <span class="u2 chat">cool! where exactly will you be?</span>
                     <span class="u1 chat">up in the ridgway, mt. horab area</span>
 
+                    {/* User Listening */}
                     <span class="u2 chat">
                         <span class="is-typing">
                             <span class="jump1"></span>
                             <span class="jump2"></span>
                             <span class="jump3"></span>
-                            {/* <span class="jump4"></span> */}
-                            {/* <span class="jump5"></span> */}
+                            <span class="jump4"></span>
+                            <span class="jump5"></span>
                         </span>
                     </span>
+
+                    {/* ChatBot Processing */}
                     <span class="u1 chat">
-                        <div class="is-typing">
-                            <div class="jump1"></div>
-                            <div class="jump2"></div>
-                            <div class="jump3"></div>
-                            <div class="jump4"></div>
-                            <div class="jump5"></div>
-                        </div>
+                        <span class="is-typing">
+                            <span class="jump1"></span>
+                            <span class="jump2"></span>
+                            <span class="jump3"></span>
+                            <span class="jump4"></span>
+                            <span class="jump5"></span>
+                        </span>
                     </span>
+
                 </div>
             </div>
         )
